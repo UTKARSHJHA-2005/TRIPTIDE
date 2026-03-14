b52bb9963f9f9e6e4c6a39bb8f453889deeaa798
Utkarsh Jha <jha.utkarsh2005@gmail.com>
2026-03-14 19:15:52 +0530

    poTip[__

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Userstore } from "../Userstore";
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../db";
